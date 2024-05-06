c4a2541c17f2e046ac41e7e78812fed1cdcc6e77
Jdjohar <Jdeep514@gmail.com>
2024-05-06 15:45:17 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+
+global.foodData = require('./db')(function call(err, data, CatData) {
+  // console.log(data)
+  if(err) console.log(err);
+  global.foodData = data;
+  global.foodCategory = CatData;
+})
+
+const express = require('express')
+const app = express()
+const port = 5000
+var path = require('path');
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://foodcareerengine.vercel.app");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+// set path for static assets
+app.use(express.static(path.join(__dirname, 'uploads')));
+
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.use('/api/auth', require('./Routes/Auth'));
+
+app.listen(port, () => {
+  console.log(`Example app listening on http://localhost:${port}`)
+})
+
